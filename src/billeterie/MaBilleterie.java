e2bc4cc856837a5b0ef4852c6327acfc45d2d34b
Appoline et sa famille Troulay <troulay.appoline@gmail.com>
2018-03-15 11:40:44 +0100

    J'ai rajouté un s.o.p "Bienvenue"

--- src/billeterie/MaBilleterie.java
+++ src/billeterie/MaBilleterie.java
@@ -4,6 +4,7 @@ package billeterie;
 public class MaBilleterie {
 
 	public static void main(String[] args) {
+		System.out.println("Bienvenue sur la billetterie en ligne !");
 		Fenetre fen=new Fenetre();
 	}
 
